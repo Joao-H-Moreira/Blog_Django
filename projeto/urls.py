ce7d8b532ded21ac5d89d68cc16f70ca12328038
João Moreira <110946307+Joao-H-Moreira@users.noreply.github.com>
2025-08-07 19:30:23 -0300

    Add files via upload

---
+++ projeto/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include
+# Importe settings e static para servir arquivos de mídia no desenvolvimento
+from django.conf import settings
+from django.conf.urls.static import static
+urlpatterns = [
+path('admin/', admin.site.urls),
+path('recipes/', include('recipes.urls')),
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
---
+++ recipes/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=200)
+    cooking_time = models.IntegerField(help_text="Tempo em minutos")
+    ingredients = models.TextField()
+    instructions = models.TextField()
+
+    image = models.ImageField(upload_to='recipes/', blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+
+        verbose_name_plural = "Recipes"
