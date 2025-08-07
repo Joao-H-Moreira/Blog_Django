fd09c7e2a2be06ae12d3470982221fb3b81b6fae
Joao-H-Moreira <joao.ham2001@gmail.com>
2025-08-07 20:49:39 -0300

    NUN SEI SOCORRO

--- recipes/models.py
+++ recipes/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Recipe(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=255)
     film_time = models.IntegerField(help_text="Tempo em minutos")
     resume = models.TextField()
     image = models.ImageField(upload_to='recipes/', blank=True, null=True)
